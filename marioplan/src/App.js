b9abf69eb6878933a540bb4bed5483442fc49f76
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2020-12-21 12:42:44 +0500

    Added routing

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {Layout} from 'antd'
 import NavBar from './components/header/NavBar'
+import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
+import CreateProject from './components/projects/CreateNewProject'
+
 function App() {
   return (
-    <BrowserRouter>
+    
         <div className="App">
           <Layout>
+          <BrowserRouter>
               <NavBar />
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/project/:id" component={ProjectDetails}/>
+                <Route path="/project/createproject" component={CreateProject} />
+              </Switch>   
+          </BrowserRouter>
           </Layout>
-
-
         </div>
-    
-    </BrowserRouter>
-
   );
 }
 
 export default App;
+
