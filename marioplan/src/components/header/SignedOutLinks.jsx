33e9d6bc7634623f4f884d5ff2c1edb5df87f642
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2020-12-21 12:45:36 +0500

    Added SignOut links Component

--- marioplan/src/components/header/SignedOutLinks.jsx
+++ marioplan/src/components/header/SignedOutLinks.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Menu } from 'antd';
-import {Link} from 'react-router-dom'
+import SignIn from '../auth/SignIn'
+import SignUp from '../auth/SignUp'
 
+const renderActions = (key, showSignInModal, setSignInShowModal, showSignUpModal, setSignUpShowModal) => {
+    console.log(key)
+        switch(key) {
+            case "login" :
+                return <SignIn showSignInModal={showSignInModal} setSignInShowModal={setSignInShowModal}/>
+            case "signup" :
+                return <SignUp showSignUpModal={showSignUpModal} setSignUpShowModal={setSignUpShowModal} />
+            default:
+        }
+}
 
 const SignedOutLinks = () =>{
+    const [showSignInModal, setSignInShowModal] = useState(false);
+    const [showSignUpModal, setSignUpShowModal] = useState(false)
+    const [currentKey, setCurrentKey] = useState('');
     return(
-        <Menu>
-            <Menu.Item>
-                <Link to="/">Signup</Link>
+        <>
+        <Menu className="nav-menu" onClick={e=>setCurrentKey(e.key)}>
+            <Menu.Item className="nav-menu-item" key="signup" onClick={()=>setSignUpShowModal(!showSignUpModal)} >
+                Signup
             </Menu.Item>
-            <Menu.Item>
-                <Link to="/">Login</Link>
+            <Menu.Item className="nav-menu-item" key="login" onClick={()=>setSignInShowModal(!showSignInModal)}>
+                Login
             </Menu.Item>
         </Menu>
+        {renderActions(currentKey, showSignInModal,setSignInShowModal, showSignUpModal, setSignUpShowModal)}
+            </> 
     );
 };
 
-export default SignedOutLinks;
+export default SignedOutLinks; 
