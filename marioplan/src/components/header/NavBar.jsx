0ece055d518614594637dc7b0563c4342a3ed0ea
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2020-12-21 12:44:26 +0500

    Added header component

--- marioplan/src/components/header/NavBar.jsx
+++ marioplan/src/components/header/NavBar.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import {Layout} from 'antd'
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import './styles.scss'
 
 const {Header} = Layout
 const NavBar = () =>{
     return(
-        <Header>
-            <h1>MarioPlan</h1>
-            <SignedInLinks />
-            <SignedOutLinks />
+        <Header className="nav-header">
+            <h1 className="nav-title">MarioPlan</h1>
+            <div className="nav-actions">
+                <SignedInLinks />
+                <SignedOutLinks />
+            </div>
+
         </Header>
     )
 }
