32ff2b025a2667bf0d7b7a3b89de68cf93320b46
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-01-06 01:51:41 +0500

    feat: Added signout functionality

--- marioplan/src/components/auth/SignUp.jsx
+++ marioplan/src/components/auth/SignUp.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button, Modal } from 'antd';
 
 const SignUp = ({showSignUpModal, setSignUpShowModal}) =>{
     console.log(showSignUpModal)
+    const [singUpUserName, setSignUpUserName] = useState('')
+    const [signUpPassword, setSignUpPassword] = useState('')
+    const [signUpFirstName, setSignUpFirstName] = useState('')
+    const [signUpLastName, setSignUpLastName] =useState('')
     return(
         <Modal 
         visible={showSignUpModal}
@@ -15,28 +19,28 @@ const SignUp = ({showSignUpModal, setSignUpShowModal}) =>{
                 name="username"
                 rules={[{ required: true, message: 'Please input your username!' }]}
                 >
-                <Input  type="email"/>
+                <Input  type="text" name='username' value={singUpUserName} onChange={e=> setSignUpUserName(e.target.value)}/>
                 </Form.Item>
                 <Form.Item
                 label="Password"
                 name="password"
                 rules={[{ required: true, message: 'Please input your password!' }]}
                 >
-                <Input.Password />
+                <Input.Password type='password' name='password' value={signUpPassword} onChange={e=>setSignUpPassword(e.target.value)}/>
                 </Form.Item>
                 <Form.Item
                 label="First Name"
                 name="firstname"
                 rules={[{ required: true, message: 'Please input your first name!' }]}
                 >
-                <Input type="text" />
+                <Input type="text" name='firstName' value={signUpFirstName} onChange={e=>setSignUpFirstName(e.target.value)}/>
                 </Form.Item>
                 <Form.Item
                 label="Last Name"
                 name="lastName"
                 rules={[{ required: true, message: 'Please input your last name!' }]}
                 >
-                <Input type="text" />
+                <Input type="text" name='lastName' value={signUpLastName} onChange={e=>setSignUpLastName(e.target.value)}/>
                 </Form.Item>
                 <Form.Item >
                 <Button type="primary" htmlType="submit">
--- marioplan/src/components/config/fbConfig.js
+++ marioplan/src/components/config/fbConfig.js
@@ -1,6 +1,7 @@
   
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth'
   
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
